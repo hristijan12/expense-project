aa00b2e2aed5061bba0e3acb60f9d8e72ccab55b
hristijan12 <hristijan.taseski@yahoo.com>
2020-02-24 15:57:23 +0100

    update

---
+++ client/build/precache-manifest.118880a6ccf97f69b846d474f5a87124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9312633eae00ef5a147821cbbf53dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "168fca2a4753187c3663",
+    "url": "/static/css/main.2c022386.chunk.css"
+  },
+  {
+    "revision": "8fcfe3d85b888132e466",
+    "url": "/static/js/2.fca637ca.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.fca637ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "168fca2a4753187c3663",
+    "url": "/static/js/main.02b5ee30.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
